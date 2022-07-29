6becd9660431e8d8618b2fbad1b38810b64f93f6
C-Iannotti <conneriannotti@outlook.com>
2022-07-29 16:56:51 -0400

    Finished implementing sales-timeframe API route Updated route to better use database connection. Added a library to handle CORS headers for data in POST requests. Fixed retrieval of data in POST requests. Changed route to accept the date after expected time frame instead of the first date. Updated query to use the calculated beginning data and end date. Updated React app to use inputs for end date and time frame. Updated React app to display the results.

--- v2/app.py
+++ v2/app.py
@@ -1,6 +1,8 @@
 import os
 import datetime
+from dateutil import parser, relativedelta
 from flask import Flask, send_from_directory, make_response, request
+from flask_cors import CORS
 from database import db_session, init_db
 from models import Sales
 from dotenv import load_dotenv
@@ -8,9 +10,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__, static_folder="client/build", template_folder="client/build")
+cors = CORS(app)
+
 @app.get("/", defaults={"path": ""})
 @app.get("/<path:path>")
 def home(path):
+    print("Why")
     if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         res = make_response(send_from_directory(app.static_folder, path))
         return res
@@ -26,72 +31,62 @@ def login():
 def get_first_sale():
     return Sales.query.first().toJSON()
 
-@app.get("/api/sales-timeframe")
+@app.post("/api/sales-timeframe")
 def get_sales_timeframe():
-    body = request.form
-    beg_date = datetime.datetime.now()
-    errors = []
-    timeframe_unit = "day"
-    timeframe = 30
-    if "timeframe" not in body:
-        pass
-    elif body["timeframe"] == "day":
-        timeframe = 1
-    elif body["timeframe"] == "week":
-        timeframe = 7
-    elif body["timeframe"] == "month":
+    with db_session.connection() as conn:
+        body = request.get_json()
+        end_date = datetime.date.today()
+        errors = []
         timeframe_unit = "month"
         timeframe = 1
-    elif body["timeframe"] == "quarter":
-        timeframe_unit = "month"
-        timeframe = 3
-    elif body["timeframe"] == "year":
-        timeframe_unit = "year"
-        timeframe = 1
+        if "timeframeUnit" not in body:
+            pass
+        elif body["timeframeUnit"] == "day":
+            timeframe = 1
+        elif body["timeframeUnit"] == "week":
+            timeframe = 7
+        elif body["timeframeUnit"] == "month":
+            timeframe_unit = "month"
+            timeframe = 1
+        elif body["timeframeUnit"] == "quarter":
+            timeframe_unit = "month"
+            timeframe = 3
+        elif body["timeframeUnit"] == "year":
+            timeframe_unit = "year"
+            timeframe = 1
 
-    if "begDate" in body:
-        try:
-            with db_session.connection() as conn:
-                results = {}
-                query_results = conn.execute("""
-                    SELECT "ProductLine", "Gender", SUM("Quantity") as Quantity, SUM("GrossIncome") as GrossIncome
-                    FROM "Sales"
-                    GROUP BY "ProductLine", "Gender"
-                    ORDER BY "ProductLine" ASC, GrossIncome DESC
-                """)
-                for item in query_results:
-                    if item[0] not in results:
-                        results[item[0]] = {}
-                    results[item[0]][item[1]] = item[2:]
-                res = make_response(results)
-                res.headers.add("Access-Control-Allow-Origin", "*")
-                return res
-        except:
-            errors.append("Passed Invalid Date")
-    elif os.getenv("DATA") == "Testing":
-        with db_session.connection() as conn:
-            beg_date = conn.execute('SELECT "Date", MAX("Time") FROM "Sales" GROUP BY "Date" ORDER BY "Date" DESC LIMIT 1').first()
-            if timeframe_unit == "day":
-                beg_date = datetime.datetime.combine(beg_date[0], beg_date[1]) - datetime.timedelta(days=timeframe)
-            elif timeframe_unit == "month":
-                beg_date = datetime.datetime.combine(beg_date[0], beg_date[1]) - datetime.timedelta(months=timeframe)
-            elif timeframe_unit == "year":
-                beg_date = datetime.datetime.combine(beg_date[0], beg_date[1]) - datetime.timedelta(years=timeframe)
+        if "endDate" in body:
+            try:
+                end_date = parser.parse(body["endDate"]).date()
+            except:
+                errors.append("Passed Invalid Date")
+        elif os.getenv("DATA") == "Testing":
+            end_date = conn.execute('SELECT "Date" FROM "Sales" GROUP BY "Date" ORDER BY "Date" DESC LIMIT 1').first()[0]
 
-            results = {}
-            query_results = conn.execute("""
-                SELECT "ProductLine", "Gender", SUM("Quantity") as Quantity, SUM("GrossIncome") as GrossIncome
-                FROM "Sales"
-                GROUP BY "ProductLine", "Gender"
-                ORDER BY "ProductLine" ASC, GrossIncome DESC
-            """)
-            for item in query_results:
-                if item[0] not in results:
-                    results[item[0]] = {}
-                results[item[0]][item[1]] = item[2:]
-            res = make_response(results)
-            res.headers.add("Access-Control-Allow-Origin", "*")
-            return res
+        beg_date = datetime.date.today()
+        if timeframe_unit == "day":
+            beg_date = end_date - relativedelta.relativedelta(days=timeframe)
+        elif timeframe_unit == "month":
+            beg_date = end_date - relativedelta.relativedelta(months=timeframe)
+        elif timeframe_unit == "year":
+            beg_date = end_date - relativedelta.relativedelta(years=timeframe)
+        end_date = end_date - relativedelta.relativedelta(days=1)
+        results = {}
+        
+        query_results = conn.execute(f"""
+            SELECT "ProductLine", "Gender", SUM("Quantity") as Quantity, SUM("GrossIncome") as GrossIncome
+            FROM "Sales"
+            WHERE "Date" Between '{beg_date}' AND '{end_date}'
+            GROUP BY "ProductLine", "Gender"
+            ORDER BY "ProductLine" ASC, GrossIncome DESC
+        """)
+        for item in query_results:
+            if item[0] not in results:
+                results[item[0]] = {}
+            results[item[0]][item[1]] = { "quantity": item[2], "grossIncome": float(item[3])}
+        res = make_response(results)
+        res.headers.add("Access-Control-Allow-Origin", "*")
+        return res
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
--- v2/client/src/Home.js
+++ v2/client/src/Home.js
@@ -6,27 +6,49 @@ class Home extends React.Component {
         super(props)
         this.state = {}
         this.getSalesData = this.getSalesData.bind(this);
+        this.handleGetSalesData = this.handleGetSalesData.bind(this);
     }
 
-    getSalesData() {
-        console.log(process.env["REACT_APP_SERVER_URL"] + process.env["REACT_APP_SALES_TIMEFRAME_PATH"])
+    getSalesData(endDate=undefined, timeframeUnit=undefined) {
         axios({
-            method: "get",
-            url: process.env["REACT_APP_SERVER_URL"] + process.env["REACT_APP_SALES_TIMEFRAME_PATH"],
+            method: "post",
+            url: process.env["REACT_APP_SERVER_URL"] + process.env["REACT_APP_SALES_TIMEFRAME_PATH"], 
+            data: {
+                endDate: endDate,
+                timeframeUnit: timeframeUnit
+            }
         })
         .then(res => {
             console.log(res)
+            this.setState({sales: res.data})
         })
         .catch(err => {
             console.error(err)
         })
     }
 
+    handleGetSalesData() {
+        let endDate = document.getElementById("end-date-input").value
+        let timeframeUnit = document.getElementById("timeframe-unit-input").value
+        this.getSalesData(endDate, timeframeUnit)
+    }
+
     render() {
-        this.getSalesData();
         return (
             <div id="home-page" className="home-page">
                 <p>This do be the home page</p>
+                <input id="end-date-input" name="end-date-input" type="date" />
+                <select id="timeframe-unit-input" name="timeframe-unit-input" defaultValue="default">
+                    <option value="default">30 days</option>
+                    <option value="day">1 day</option>
+                    <option value="week">1 week</option>
+                    <option value="month">1 month</option>
+                    <option value="quarter">1 yearly-quarter</option>
+                    <option value="year">1 year</option>
+                </select>
+                <button type="button" onClick={this.handleGetSalesData}>Submit</button>
+                <br />
+                {this.state.sales && JSON.stringify(this.state.sales)}
             </div>
         )
     }
